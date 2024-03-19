dcc58d7d717f53861e588bc0765cf29369ba61e5
nvineng <naveen211063cse@sdbc.ac.in>
2024-03-19 20:13:49 +0530

    project reform and add something new

--- src/pages/Projects/Projects.js
+++ src/pages/Projects/Projects.js
@@ -16,17 +16,17 @@ const Projects = () => {
                 <h2 className="col-12 mt-3 mb-1 text-center text-uppercase">Projects</h2>
                 <hr />
                 <p className="pb-3 text-center">
-                    👉 Here are my top 3 recent projects with live links
+                    👉 Here are my top  some of the recent projects with live links
                     and source code</p>
                 {/* card design  */}
                 <div className="row" id="ads">
                     {/* project1 */}
                     <Fade right>
 
-                        <div className="col-md-4">
+                        <div className="col-md-4 ">
                             <div className="card rounded">
                                 <div className="card-image">
-                                    <span className="card-notify-badge">Full Stack</span>
+                                    <span className="card-notify-badge">Full_Stack</span>
                                     <img src={img1} alt="project1" />
 
                                 </div>
@@ -35,13 +35,25 @@ const Projects = () => {
                                     <span className="card-detail-badge">Express</span>
                                     <span className="card-detail-badge">React</span>
                                     <span className="card-detail-badge">Mongodb</span>
+
                                 </div>
-                                <div className="card-body text-center">
+                                <div className="card-body ">
                                     <div className="ad-title m-auto">
-                                        <h5 className="text-uppercase">
+                                        <h5 className="text-uppercase text-center">
                                             My Portfolio WebApp
                                         </h5>
-                                        <a className="ad-btn text-uppercase" href="#" >View</a>
+
+
+                                        <div class="container mt-2">
+                                            <div class="row">
+                                                <div class="col-md-6">
+                                                    <a href="#" class=" ad-btn  text-uppercase custom-btn1" target="_blank" rel="noreferrer">VIEW</a>
+                                                </div>
+                                                <div class="col-md-6">
+                                                    <a href="https://github.com/mishra-naveen-dev/portfolio-mern-frontend" class=" ad-btn  text-uppercase  custom-btn2" target="_blank" rel="noreferrer">CODE</a>
+                                                </div>
+                                            </div>
+                                        </div>
                                     </div>
                                 </div>
                             </div>
@@ -50,7 +62,7 @@ const Projects = () => {
                     </Fade>
                     {/* project2 */}
                     <Fade bottom>
-                        <div className="col-md-4">
+                        <div className="col-md-4 ">
                             <div className="card rounded">
                                 <div className="card-image">
                                     <span className="card-notify-badge">JavaScript Project</span>
@@ -63,12 +75,21 @@ const Projects = () => {
                                     <span className="card-detail-badge">JavaScript</span>
                                     <span className="card-detail-badge">API</span>
                                 </div>
-                                <div className="card-body text-center">
+                                <div className="card-body ">
                                     <div className="ad-title m-auto">
-                                        <h5 className="text-uppercase">
+                                        <h5 className="text-uppercase text-center">
                                             Analog Clock With Current Time
                                         </h5>
-                                        <a className="ad-btn text-uppercase" href="https://mishra-naveen-dev.github.io/analog_clock/" target="_blank" rel="noreferrer" >View</a>
+                                        <div class="container mt-2">
+                                            <div class="row">
+                                                <div class="col-md-6">
+                                                    <a href="https://mishra-naveen-dev.github.io/analog_clock/" class=" ad-btn  text-uppercase custom-btn1" target="_blank" rel="noreferrer">VIEW</a>
+                                                </div>
+                                                <div class="col-md-6">
+                                                    <a href="https://github.com/mishra-naveen-dev/analog_clock" class=" ad-btn  text-uppercase  custom-btn2" target="_blank" rel="noreferrer">CODE</a>
+                                                </div>
+                                            </div>
+                                        </div>
                                     </div>
                                 </div>
                             </div>
@@ -77,7 +98,7 @@ const Projects = () => {
                     </Fade>
                     {/* project3 */}
                     <Fade left>
-                        <div className="col-md-4">
+                        <div className="col-md-4 mx-auto">
                             <div className="card rounded">
                                 <div className="card-image">
                                     <span className="card-notify-badge">JavaScript Project</span>
@@ -88,14 +109,23 @@ const Projects = () => {
                                     <span className="card-detail-badge">HTML</span>
                                     <span className="card-detail-badge">CSS</span>
                                     <span className="card-detail-badge">JavaScript</span>
-                                    {/* <span className="card-detail-badge">Mongodb</span> */}
+
                                 </div>
-                                <div className="card-body text-center">
+                                <div className="card-body ">
                                     <div className="ad-title m-auto">
-                                        <h5 className="text-uppercase">
+                                        <h5 className="text-uppercase text-center">
                                             Login & Resgistration Form
                                         </h5>
-                                        <a className="ad-btn text-uppercase" href="https://mishra-naveen-dev.github.io/Login-registration-page/?%23=&%23=&%23=" target="_blank" rel="noreferrer" >View</a>
+                                        <div class="container mt-2">
+                                            <div class="row">
+                                                <div class="col-md-6">
+                                                    <a href="https://mishra-naveen-dev.github.io/Login-registration-page/" class=" ad-btn  text-uppercase custom-btn1" target="_blank" rel="noreferrer">VIEW</a>
+                                                </div>
+                                                <div class="col-md-6">
+                                                    <a href="https://github.com/mishra-naveen-dev/Login-registration-page" class=" ad-btn  text-uppercase  custom-btn2" target="_blank" rel="noreferrer">CODE</a>
+                                                </div>
+                                            </div>
+                                        </div>
                                     </div>
                                 </div>
                             </div>
@@ -105,7 +135,7 @@ const Projects = () => {
 
                     {/* project4 */}
                     <Fade left>
-                        <div className="col-md-4">
+                        <div className="col-md-4  mt-3">
                             <div className="card rounded">
                                 <div className="card-image">
                                     <span className="card-notify-badge">Netflix Clone Project</span>
@@ -118,12 +148,21 @@ const Projects = () => {
                                     <span className="card-detail-badge">JavaScript</span>
 
                                 </div>
-                                <div className="card-body text-center">
+                                <div className="card-body ">
                                     <div className="ad-title m-auto">
-                                        <h5 className="text-uppercase">
+                                        <h5 className="text-uppercase  text-center">
                                             Netflix Clone Project
                                         </h5>
-                                        <a className="ad-btn text-uppercase" href="https://mishra-naveen-dev.github.io/Netflix_Clone/" target="_blank" rel="noreferrer" >View</a>
+                                        <div class="container mt-2">
+                                            <div class="row">
+                                                <div class="col-md-6">
+                                                    <a href="https://mishra-naveen-dev.github.io/Netflix_Clone/" class=" ad-btn  text-uppercase custom-btn1" target="_blank" rel="noreferrer">VIEW</a>
+                                                </div>
+                                                <div class="col-md-6">
+                                                    <a href="https://github.com/mishra-naveen-dev/Netflix_Clone" class=" ad-btn  text-uppercase  custom-btn2" target="_blank" rel="noreferrer">CODE</a>
+                                                </div>
+                                            </div>
+                                        </div>
                                     </div>
                                 </div>
                             </div>
@@ -133,7 +172,7 @@ const Projects = () => {
 
                     {/* project5 */}
                     <Fade left>
-                        <div className="col-md-4">
+                        <div className="col-md-4 mt-3">
                             <div className="card rounded">
                                 <div className="card-image">
                                     <span className="card-notify-badge">Tic-Tac-Toe-game</span>
@@ -146,12 +185,61 @@ const Projects = () => {
                                     <span className="card-detail-badge">JavaScript</span>
 
                                 </div>
-                                <div className="card-body text-center">
+                                <div className="card-body">
                                     <div className="ad-title m-auto">
-                                        <h5 className="text-uppercase">
+                                        <h5 className="text-uppercase  text-center">
                                             Tic-Tac-Toe-game
                                         </h5>
-                                        <a className="ad-btn text-uppercase" href="https://mishra-naveen-dev.github.io/Tic-Tac-Toe-game-/" target="_blank" rel="noreferrer" >View</a>
+                                        <div class="container mt-2">
+                                            <div class="row">
+                                                <div class="col-md-6">
+                                                    <a href="https://mishra-naveen-dev.github.io/Tic-Tac-Toe-game-/" class=" ad-btn  text-uppercase custom-btn1" target="_blank" rel="noreferrer">VIEW</a>
+                                                </div>
+                                                <div class="col-md-6">
+                                                    <a href="https://github.com/mishra-naveen-dev/Tic-Tac-Toe-game-" class=" ad-btn  text-uppercase  custom-btn2" target="_blank" rel="noreferrer">CODE</a>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+
+                        </div>
+                    </Fade>
+
+                    {/* project6 */}
+                    <Fade left>
+                        <div className="col-md-4 mt-3">
+                            <div className="card rounded">
+                                <div className="card-image">
+                                    <span className="card-notify-badge">FULL_STACK PROJECT</span>
+                                    <img src={"img not out ongoing project"} alt="" />
+
+                                </div>
+                                <div className="card-image-overly m-auto mt-4">
+                                    <span className="card-detail-badge">HTML</span>
+                                    <span className="card-detail-badge">CSS</span>
+                                    <span className="card-detail-badge">JavaScript</span>
+                                    <span className="card-detail-badge">BOOTSTRAP5</span>
+                                    <span className="card-detail-badge">PYTHON-DJANGO</span>
+
+                                </div>
+                                <div className="card-body">
+                                    <div className="ad-title m-auto">
+                                        <h5 className="text-uppercase  text-center">
+                                            Learning Management System
+                                        </h5>
+                                        {/* <a className="ad-btn text-uppercase" href="https://mishra-naveen-dev.github.io/Tic-Tac-Toe-game-/" target="_blank" rel="noreferrer" >View</a> */}
+                                        <div class="container mt-2">
+                                            <div class="row">
+                                                <div class="col-md-6">
+                                                    <a href="#" class=" ad-btn  text-uppercase custom-btn1" target="_blank" rel="noreferrer">VIEW</a>
+                                                </div>
+                                                <div class="col-md-6">
+                                                    <a href="https://github.com/mishra-naveen-dev/django-lms-project" class=" ad-btn  text-uppercase  custom-btn2" target="_blank" rel="noreferrer">CODE</a>
+                                                </div>
+                                            </div>
+                                        </div>
                                     </div>
                                 </div>
                             </div>
